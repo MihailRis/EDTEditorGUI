96ed7765d669717dfab5cf2e86bd74a248059e70
ubuntu <mexaruc@gmail.com>
2022-09-12 10:24:48 +0300

    Added run with file feature

--- src/main/java/mihailris/edteditorgui/Main.java
+++ src/main/java/mihailris/edteditorgui/Main.java
@@ -1,13 +1,22 @@
 package mihailris.edteditorgui;
 
 import mihailris.edteditorgui.utils.EditorSwingUtils;
+import mihailris.edtfile.EDT;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         EditorSwingUtils.configTheme();
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         MainFrame mainFrame = (MainFrame) context.getBean("mainFrame");
+        if (args.length > 0){
+            File file = new File(args[0]);
+            mainFrame.context.setRoot(EDT.read(Files.readAllBytes(file.toPath())));
+        }
         mainFrame.launch();
     }
 }
