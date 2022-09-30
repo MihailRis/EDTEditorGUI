6bdbdc583414d1216f2a027054d62724fe96d6ac
MihailRis <mexaruc@gmail.com>
2022-09-30 23:19:03 +0300

    Added javadocs for DialogsUtil

--- src/main/java/mihailris/edteditorgui/utils/DialogsUtil.java
+++ src/main/java/mihailris/edteditorgui/utils/DialogsUtil.java
@@ -4,6 +4,10 @@ import java.awt.*;
 import java.io.File;
 
 public class DialogsUtil {
+    /**
+     * Show file open dialog
+     * @return file or null if cancelled
+     */
     public static File chooseOpenFile(Frame frame){
         FileDialog fileChooser = new FileDialog(frame);
         fileChooser.setVisible(true);
@@ -14,6 +18,10 @@ public class DialogsUtil {
         return new File(directory, filename);
     }
 
+    /**
+     * Show file save dialog
+     * @return file or null if cancelled
+     */
     public static File chooseSaveFile(Frame frame){
         FileDialog fileChooser = new FileDialog(frame);
         fileChooser.setMode(FileDialog.SAVE);
