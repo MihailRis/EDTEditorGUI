88a3fa93d24ec59e1b1d40613467a8de04084583
MihailRis <mexaruc@gmail.com>
2022-10-05 22:50:33 +0300

    Minor refactor 2

--- src/main/java/mihailris/edteditorgui/uicomponents/EDTTreeCellRenderer.java
+++ src/main/java/mihailris/edteditorgui/uicomponents/EDTTreeCellRenderer.java
@@ -25,50 +25,53 @@ public class EDTTreeCellRenderer extends DefaultTreeCellRenderer {
     }
 
     @Override
-    public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded,
-                                                  boolean leaf, int row, boolean hasFocus) {
+    public Component getTreeCellRendererComponent(JTree tree,
+                                                  Object value,
+                                                  boolean sel,
+                                                  boolean expanded,
+                                                  boolean leaf,
+                                                  int row,
+                                                  boolean hasFocus) {
         super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
         DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
         EDTNodeUserData userObject = (EDTNodeUserData) node.getUserObject();
         String key = userObject.getTag();
-        {
-            final Object edtValue = userObject.getValue();
-            String valueString = String.valueOf(edtValue);
-            if (valueString.length() > 40){
-                valueString = valueString.substring(0, 40) + "...";
-            }
-            EDTItem parentNode = userObject.getParent();
-            boolean isListItem = false;
-            if (parentNode != null) {
-                if (parentNode instanceof EDTList)
-                    isListItem = true;
-            }
-            if (isListItem){
-                key = String.format(SPAN_FORMAT, "gray", "["+userObject.getIndex()+"]");
-                if (userObject.getTag() != null)
-                    key += " "+userObject.getTag();
-            } else if (!(edtValue instanceof EDTItem)){
-                key += ":";
-            }
-            String text;
-            if (!(edtValue instanceof EDTItem)) {
-                if (edtValue instanceof String) {
-                    valueString = "\"" + valueString + "\"";
-                }
-                if (edtValue instanceof byte[]) {
-                    valueString = "byte["+((byte[]) edtValue).length+"]";
-                }
-                text = key+" <b>"+String.format(SPAN_FORMAT, "#50A040", valueString)+"</b>";
-            } else {
-                if (((EDTItem) edtValue).size() == 0){
-                    text = key + ": " + String.format(SPAN_FORMAT, "gray", "empty "+(edtValue instanceof EDTGroup ? "group" : "list"));
-                }
-                else {
-                    text = key;
-                }
-            }
-            this.setText("<html>" + text + "</html>");
+        final Object edtValue = userObject.getValue();
+        String valueString = String.valueOf(edtValue);
+        if (valueString.length() > 40){
+            valueString = valueString.substring(0, 40) + "...";
         }
+        EDTItem parentNode = userObject.getParent();
+        boolean isListItem = false;
+        if (parentNode != null) {
+            if (parentNode instanceof EDTList) {
+                isListItem = true;
+            }
+        }
+        if (isListItem){
+            key = String.format(SPAN_FORMAT, "gray", "["+userObject.getIndex()+"]");
+            if (userObject.getTag() != null) {
+                key += " " + userObject.getTag();
+            }
+        } else if (!(edtValue instanceof EDTItem)){
+            key += ":";
+        }
+        String text;
+        if (!(edtValue instanceof EDTItem)) {
+            if (edtValue instanceof String) {
+                valueString = "\"" + valueString + "\"";
+            } else if (edtValue instanceof byte[]) {
+                valueString = "byte["+((byte[]) edtValue).length+"]";
+            }
+            text = key+" <b>"+String.format(SPAN_FORMAT, "#50A040", valueString)+"</b>";
+        } else {
+            if (((EDTItem) edtValue).size() == 0){
+                text = key + ": " + String.format(SPAN_FORMAT, "gray", "empty "+(edtValue instanceof EDTGroup ? "group" : "list"));
+            } else {
+                text = key;
+            }
+        }
+        this.setText("<html>" + text + "</html>");
         return this;
     }
 }
